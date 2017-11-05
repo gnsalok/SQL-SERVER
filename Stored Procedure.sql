9bd3f506c93c5be8c1662d0d35283baafa150699
Alok Tripathi <gnsalok@gmail.com>
2017-11-05 21:52:13 +0530

    attempting a question on sql Stored Procedure.sql

--- Stored Procedure.sql
+++ Stored Procedure.sql
@@ -2,18 +2,51 @@ use sample;
 select * from tblEmployee
 
 Declare @currentTime datetime
-exec  selectEmployee 'John' ,3, @currentTime output
+exec  selectEmployee 'John' ,1, @currentTime output
 print @currentTime
 
 ALTER PROCEDURE selectEmployee
 @EmployeeName nvarchar(50),
 @EmployeeId int,
-@CurrentDateAndTime datetime output		--output parameter
+@CurrentTime datetime output		--output parameter
 AS 
 BEGIN
-Set @CurrentDateAndTime= Getdate()
-
-Select * from tblEmployee where Name=@EmployeeName  and
+Set @CurrentTime= Getdate()
+Select * from tblEmployee where Name=@EmployeeName
+AND
 Id=@EmployeeId
-
 END
+
+
+
+
+
+--Ques :- WASp which takes Gender as input and returns total salary
+
+
+
+CREATE PROCEDURE spGetSalaryByGender
+@empGender nvarchar(50),
+@empSalary int output
+As
+Begin
+SET @empSalary = (select sum(salary) from tblEmployee
+					where Gender=@empGender)
+SELECT NAME,SALARY 
+FROM tblEmployee 
+WHERE Gender=@empGender
+
+End 
+
+--try to execute
+Declare @empSalary int
+Execute spGetSalaryByGender 'Male',@empSalary output
+print @empSalary
+
+
+
+
+
+
+
+
